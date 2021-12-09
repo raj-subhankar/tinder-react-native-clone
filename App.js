f23374765c002336de8a9d8c435bb5a5790665db
raj-subhankar <subhankar.rj@gmail.com>
2021-12-09 19:51:50 +0530

    Initial setup

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+ import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import tw from "tailwind-rn"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw("flex-1 justify-center items-center")}>
+      <Text>Hello</Text>
+      <Button title='Click me' />
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
